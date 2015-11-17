dbf1851746127818ba7c3fa67d8ed01c3a07bb45
Zohaib Zahid <zohaib.mir@gmail.com>
2015-11-17 15:10:22 +0100

    add function for getting products of a category

--- app/code/local/M4U/SortProducts/Block/SortProducts.php
+++ app/code/local/M4U/SortProducts/Block/SortProducts.php
@@ -11,12 +11,19 @@
  *
  * @author Zohaib
  */
-class M4U_SortProducts_Block_SortProducts extends Mage_Core_Block_Template {
+class M4U_SortProducts_Block_SortProducts extends Mage_Catalog_Block_Product_Abstract {
 
     // necessary methods
 
-    public function myfunction() {
-        return "Hello from Block Function";
+    public function getProducts() {
+        $categoryid = 4;
+        $category = new Mage_Catalog_Model_Category();
+        $category->load($categoryid);
+        $collection = $category->getProductCollection();
+        $collection->addAttributeToSelect('*');       
+        $collection->addAttributeToFilter('sort_order', array('neq' => ''));
+        $collection->setOrder('sort_order', 'asc');
+        return $collection;
     }
 
 }
